2788b74703614b78c556d315e7346497fe620c0c
feriely <feriely@163.com>
2014-10-07 12:41:16 +0800

    Replace inheritance with member variable

--- echosvr.h
+++ echosvr.h
@@ -3,7 +3,7 @@
 
 #include <ace/ACE.h>
 #include <ace/Reactor.h>
-
+/*
 template <typename ACCEPTOR>
 class EchoServer : public ACCEPTOR
 {
@@ -20,5 +20,26 @@ public:
 			reactor->end_reactor_event_loop();
 	}
 };
+*/
 
+// Acceptor as a member variable
+template <typename ACCEPTOR>
+class EchoServer
+{
+public:
+	EchoServer(ACE_Reactor* reactor)
+	{
+		ACE_NEW_NORETURN(acceptor_, ACCEPTOR(reactor));
+		typename ACCEPTOR::PEER_ADDR serverAddr;
+		int result;
+
+		result = serverAddr.set("9877", (ACE_UINT32)INADDR_ANY);
+		if (result != -1)
+			result = acceptor_->open(serverAddr);
+		if (result == -1)
+			reactor->end_reactor_event_loop();
+	}
+private:
+	ACCEPTOR *acceptor_;
+};
 #endif
